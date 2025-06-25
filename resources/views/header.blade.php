87b1569d8e99a6dc9d241c6ffc890f892b8d32a0
Ciara <ciara@stats4sd.org>
2025-06-25 11:49:28 +0100

    removes bold font

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -23,7 +23,7 @@
             <ul class="flex space-x-6 xl:space-x-10 font-medium text-sm xl:text-base ">
                 <!-- What We Do Dropdown -->
                 <li class="relative nav-item group">
-                    <a href="/we-do" class="nav-link uppercase font-semibold flex items-center space-x-1">
+                    <a href="/we-do" class="nav-link uppercase flex items-center space-x-1">
                         <span>{{ t("What we do") }}</span>
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
