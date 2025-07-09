94e22f6c67799a3902b9a79d9309099b014e7200
Ciara <ciara@stats4sd.org>
2025-07-09 14:20:31 +0100

    updates blogs

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Article;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class User extends Authenticatable
 {
@@ -44,5 +46,16 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /** @return BelongsToMany<Article> */
+    public function authoredArticles(): BelongsToMany
+    {
+        return $this->belongsToMany(Article::class, 'article_user', 'user_id', 'article_id');
+    }
+
+    public function getCleanNameAttribute()
+    {
+        return str_replace(' (old)', '', $this->name);
     }
 }
