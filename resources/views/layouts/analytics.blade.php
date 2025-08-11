0037c902bda26fbe6fdea9ddfca53036d7cf192e
Dave Mills <d.e.mills@stats4sd.org>
2025-08-11 14:52:31 +0100

    update comment

--- resources/views/layouts/analytics.blade.php
+++ resources/views/layouts/analytics.blade.php
@@ -1,2 +1,2 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
+<!-- Plausible Analytics Tag -->
 <script defer data-domain="{{ \Illuminate\Support\Str::replace('https://', '', config('app.url') }}" src="https://plausible.io/js/script.js"></script>
